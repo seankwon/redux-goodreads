6c66045fb8e95d4baabda799fff935027220d4d7
Sean <Sean Kwon>
2017-02-22 21:50:43 -0500

    add tests for library reducers

--- tests/ReducerTests/libraryReducer_spec.js
+++ tests/ReducerTests/libraryReducer_spec.js
@@ -3,10 +3,39 @@ import {
 } from 'chai'
 import library from '../../public/javascripts/reducers/LibraryReducer'
 import {
-    requestBooks,
     storeBooksData,
     receiveDetailedBook
 } from '../../public/javascripts/actions/LibraryActions'
 
-describe('library reducer', () => {
+describe ('library reducer', () => {
+  it ('should handle STORE_BOOKS_DATA', () => {
+    let data = {
+      books: {id: 1, author: 'John Doe', title: 'The Generic Book'},
+      searches: {'John Doe': [1]}
+    }
+    expect (library({bookPage: {}}, storeBooksData(data, 'John Doe')))
+      .to.deep.equals({
+        books: data.books,
+        searches: data.searches,
+        bookPage: {}
+      })
+  })
+
+  it ('should handle RECEIVE_DETAILED_BOOK', () => {
+    let detailedBook = {
+      title: 'Generic Book',
+      author: 'John Doe',
+      description: 'Lorem Ipsum Delorum...',
+      id: '2'
+    }
+
+    expect (library({
+      books: {}, 
+      searches: {}
+    }, receiveDetailedBook(detailedBook, '2'))).to.deep.equals({
+      books: {},
+      searches: {},
+      bookPage: detailedBook
+    })
+  })
 })
